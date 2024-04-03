4d5ddf0a3eba925f2c2d49dd1c4063f821b8f652
lotuseana <143060902+lotuseana@users.noreply.github.com>
2024-04-03 10:08:55 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -15,8 +15,18 @@ public class Sound
    */
   public int limitAmplitude(int limit)
   {  
-    /* to be implemented in part (a) */
-    return 0;
+    int count =0;
+    for (int i =0; i<samples.length; i++){
+      if (samples[i]>limit){
+        samples[i]=limit;
+        count++;}
+      else if (samples[i]<(limit*-1)){
+        samples[i]=limit*-1;
+        count++;
+      
+      }
+    }
+    return count;
   }
 
 
